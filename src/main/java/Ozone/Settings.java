4bf00169ffb089ca34f9271fe22850f0d396771e
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2020-09-17 06:49:26 +0200

    should be false

--- src/main/java/Ozone/Settings.java
+++ src/main/java/Ozone/Settings.java
@@ -2,7 +2,7 @@ package Ozone;
 
 public class Settings {
 
-    public static boolean colorPatch = false, antiSpam = true, debugMode = true;
+    public static boolean colorPatch = false, antiSpam = true, debugMode = false;
     //public static String commandsPrefix = ",";
 
 }
