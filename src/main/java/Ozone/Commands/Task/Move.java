e5139aff52f3fdbd54cb4f766af82e8aedd5d83f
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2020-09-17 06:41:51 +0200

    wrong pos

--- src/main/java/Ozone/Commands/Task/Move.java
+++ src/main/java/Ozone/Commands/Task/Move.java
@@ -22,8 +22,8 @@ public class Move extends Task {
         destPos = new Vec2(dest.x * 8, dest.y * 8);
         destTile = new Tile(Math.round(dest.x), Math.round(dest.y));
         destTilePos = dest;
-        setTick(40);
-        Vars.player.unit().moveAt(destPos);
+        setTick(20);
+
     }
 
     @Override
@@ -42,9 +42,10 @@ public class Move extends Task {
     @Override
     public void update() {
         if (tick()) return;
-        int xx = Math.round(destTilePos.x - PlayerInterface.getCurrentPos().x);
-        int yy = Math.round(destTilePos.y - PlayerInterface.getCurrentPos().y);
-
+        int xx = Math.round(destTilePos.x - PlayerInterface.getCurrentTilePos().x);
+        int yy = Math.round(destTilePos.y - PlayerInterface.getCurrentTilePos().y);
+        Log.infoTag("Ozone-AI", "X: " + xx);
+        Log.infoTag("Ozone-AI", "Y: " + yy);
         if (Meth.positive(yy) < tolerance) yy = 0;
         else if (yy < 0) yy = -1;
         else if (yy > tolerance) yy = 1;
@@ -53,9 +54,11 @@ public class Move extends Task {
         else if (xx < 0) xx = -1;
         else if (xx > tolerance) xx = 1;
         setMov(new Vec2(xx, yy));
-
-        Log.infoTag("Ozone-AI", String.valueOf(lastDist));
         lastDist = getCurrentDistance();
+        Log.infoTag("Ozone-AI", "Dist: " + lastDist);
+        Log.infoTag("Ozone-AI", "DriveX: " + xx);
+        Log.infoTag("Ozone-AI", "DriveY: " + yy);
+
     }
 
     public void setMov(Vec2 mov) {
