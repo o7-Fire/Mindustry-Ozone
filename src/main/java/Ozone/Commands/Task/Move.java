60ccf2e1016a9b0bb93efdc30199c48ea0c66d4e
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2020-09-17 06:46:28 +0200

    debugs, peoples, debugs

--- src/main/java/Ozone/Commands/Task/Move.java
+++ src/main/java/Ozone/Commands/Task/Move.java
@@ -43,8 +43,8 @@ public class Move extends Task {
         if (tick()) return;
         int xx = Math.round(destTilePos.x - PlayerInterface.getCurrentTilePos().x);
         int yy = Math.round(destTilePos.y - PlayerInterface.getCurrentTilePos().y);
-        Log.infoTag("Ozone-AI", "X: " + xx);
-        Log.infoTag("Ozone-AI", "Y: " + yy);
+        Log.debug("Ozone-AI @", "X: " + xx);
+        Log.debug("Ozone-AI @", "Y: " + yy);
         if (Meth.positive(yy) < tolerance) yy = 0;
         else if (yy < 0) yy = -1;
         else if (yy > tolerance) yy = 1;
@@ -54,9 +54,9 @@ public class Move extends Task {
         else if (xx > tolerance) xx = 1;
         setMov(new Vec2(xx, yy));
         double lastDist = getCurrentDistance();
-        Log.infoTag("Ozone-AI", "Dist: " + lastDist);
-        Log.infoTag("Ozone-AI", "DriveX: " + xx);
-        Log.infoTag("Ozone-AI", "DriveY: " + yy);
+        Log.debug("Ozone-AI @", "Dist: " + lastDist);
+        Log.debug("Ozone-AI @", "DriveX: " + xx);
+        Log.debug("Ozone-AI @", "DriveY: " + yy);
 
     }
 
