6eaa2aad6d1653203bc75d56c42428c3bdf8b65f
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2020-09-19 11:34:24 +0200

    Strave

--- src/main/java/Main/Ozone.java
+++ src/main/java/Main/Ozone.java
@@ -17,8 +17,6 @@ import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 
 public class Ozone extends Mod {
-
-
     public final static String AtomHash = "02051bd3ca";
     public final static String AtomDownload = "https://jitpack.io/com/github/o7-Fire/Atomic-Library/Atomic/" + AtomHash + "/Atomic-" + AtomHash + ".jar";
     private static volatile boolean init = false;
