5301f1fa16d821bc9dd536a21b443671e29ca304
Itzbenz <gamerrindoubuntu@gmail.com>
2020-09-29 04:55:05 +0200

    gay

--- tools/src/main/java/UpdateLibrary.java
+++ tools/src/main/java/UpdateLibrary.java
@@ -1,4 +1,16 @@
+import Main.Ozone;
+import Ozone.Pre.Download;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class UpdateLibrary {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
+        File atomic = new File("libs/Atomic.jar");
+        atomic.delete();
+        Download download = new Download(new URL(Ozone.AtomDownload), atomic);
+        download.display();
+        download.run();
     }
 }
