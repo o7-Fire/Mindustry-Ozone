f02698d62eb9b704348a2a3aa820255789b60513
Itzbenz <gamerrindoubuntu@gmail.com>
2020-10-07 03:57:52 +0200

    yet

--- Tools/src/test/java/Obfuscate.java
+++ Tools/src/test/java/Obfuscate.java
@@ -3,12 +3,19 @@ import java.util.ArrayList;
 
 public class Obfuscate {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         File root = new File("src/main/java/Ozone/");
-        if(!root.exists())throw new RuntimeException("gay");
+        if (!root.exists()) throw new RuntimeException("gay");
         ArrayList<File> files = recurse(root);
+        for (File f : files) {
+            System.out.println(f.getAbsolutePath());
+        }
+        System.out.println("Total: " + files.size());
+        System.out.println("Commit git crime");
+        Runtime.getRuntime().exec("git commit -m \"yeet on earth, the compiler mean death\"");
+        for (File f : files) {
 
-
+        }
 
     }
 
@@ -16,7 +23,7 @@ public class Obfuscate {
         ArrayList<File> files = new ArrayList<>();
         for(File c : f.listFiles()){
             if(c.isDirectory())
-                files.addAll(recurse(f));
+                files.addAll(recurse(c));
             else
                 files.add(c);
         }
