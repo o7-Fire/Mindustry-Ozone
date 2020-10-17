491ef84ce629d9dcfb5e9547e28bd3166726e4d3
Itzbenz <gamerrindoubuntu@gmail.com>
2020-10-17 06:54:15 +0200

    wtf

--- Android/src/main/java/Main/Ozone.java
+++ Android/src/main/java/Main/Ozone.java
@@ -1,13 +1,12 @@
 package Main;
 
 
-import dalvik.system.DexClassLoader;
 import mindustry.mod.Mod;
 
 
 //Atomic library should support android, and add android mindustry example to lib
 public class Ozone extends Mod {
-    public static DexClassLoader atomLibrary, actualMods;
+
 
     public Ozone() {
 
