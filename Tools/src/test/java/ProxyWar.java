65c97653286205845ac517a295b0fd2cced7a423
Itzbenz <gamerrindoubuntu@gmail.com>
2020-10-17 06:06:27 +0200

    gay

--- Tools/src/test/java/ProxyWar.java
+++ Tools/src/test/java/ProxyWar.java
@@ -1,11 +1,6 @@
-
-
-import java.io.File;
-
 public class ProxyWar {
 
     public static void main(String[] args) throws Throwable {
-        File test = new File("Tools/src/test/java/");
         String s = "gabe itch";
         System.out.println(s);
         System.out.println("dont //remove this idiot");
