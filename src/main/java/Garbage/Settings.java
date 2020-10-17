80a4ebecea1d17e82c0bacef13f5545c5bb7a7d7
Itzbenz <gamerrindoubuntu@gmail.com>
2020-10-18 01:48:01 +0200

    gae

--- src/main/java/Garbage/Settings.java
+++ src/main/java/Garbage/Settings.java
@@ -3,7 +3,7 @@ package Garbage;
 public class Settings {
 
     public static boolean colorPatch = false, debugMode = false;
-    public static boolean antiSpam = false;
+    public static boolean antiSpam = false, logMessage = false;
     public static String commandsPrefix = ",";
 
 }
