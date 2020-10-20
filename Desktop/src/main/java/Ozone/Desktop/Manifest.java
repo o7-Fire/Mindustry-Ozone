ab4c1aa823a4892115b4f213c2aea5a1ee3ee754
Itzbenz <gamerrindoubuntu@gmail.com>
2020-10-20 09:52:36 +0200

    bruh java new File is horrible

--- Desktop/src/main/java/Ozone/Desktop/Manifest.java
+++ Desktop/src/main/java/Ozone/Desktop/Manifest.java
@@ -7,7 +7,7 @@ import arc.util.Log;
 import java.io.File;
 
 public class Manifest {
-    public static File messageLogFolder = new File("Ozone/");
+    public static File messageLogFolder = new File(Atom.Manifest.currentFolder, "Ozone/");
     public static File messageLog = new File(messageLogFolder, "MessageLogArr.dat");
     public static File messageLogBackup = new File("BackupMessageLogArr.dat");
 
